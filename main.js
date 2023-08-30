9330c326c2e7b31371fd73842b19b8fa466eb08e
SERGIOOVEP <sergio.ovejeropascual@gmail.com>
2023-08-30 09:50:55 +0200

    terminado guardar en localStorage los elementos que selecciona el usuario

--- main.js
+++ main.js
@@ -4,18 +4,44 @@ const urlCategorias = 'products/category'
 const espacioSection = document.querySelector('#idSection')
 const fragment = document.createDocumentFragment('#idSection')
 const espacioCategoria = document.querySelector('#idCatElegida')
+const divTituloCategoria = document.querySelector('#idTituloCategoria')
+const datosLocal = JSON.parse(localStorage.getItem('lista')) || [];
+const espacioBoton = document.querySelector('.btnEliminar')
+const botonSumar = document.querySelector('.botonSumaLocal')
+const h2TituloObjeto = document.querySelector('.h2Titulo')
 
 
 
 
 document.addEventListener('change', (ev) => {
 
-    if (ev.target.className = "claseCategoria"){
+    if (ev.target.className = "claseCategoria") {
         espacioCategoria.innerHTML = ""
+        divTituloCategoria.innerHTML = ""
         pintarCategoria()
+        espacioBoton.style.display = 'block'
     }
 
 })
+
+
+espacioBoton.addEventListener('click', () => {
+    borrarBusqueda()
+})
+
+
+document.addEventListener('click', (ev) => {
+    if (ev.target.className == 'btnSumaCarro') {
+        let espacioH2 = ev.target.querySelector('H2')
+        const valorH2 = espacioH2.innerHTML
+        console.log(valorH2);
+        guardarLocal(valorH2)
+    }
+
+})
+
+
+
 
 
 const consulta = async (urlPeticion) => {
@@ -60,6 +86,7 @@ const pintarTodos = async () => {
         espacioFigure.classList.add('figureElementos')
 
         const espacioTitulo = document.createElement('H2')
+        espacioTitulo.classList.add('h2Titulo')
         espacioTitulo.textContent = item.title
 
         const espacioImagen = document.createElement('IMG')
@@ -70,9 +97,12 @@ const pintarTodos = async () => {
         espacioPrecio.textContent = `Precio: ${item.price} €`
 
         const espacioBoton = document.createElement('BUTTON')
+        espacioBoton.classList.add('btnSumaCarro')
         espacioBoton.textContent = 'Añadir'
 
-        espacioFigure.append(espacioTitulo, espacioImagen, espacioPrecio, espacioBoton)
+        espacioBoton.append(espacioTitulo, espacioImagen, espacioPrecio)
+
+        espacioFigure.append(espacioBoton)
 
         fragment.append(espacioFigure)
 
@@ -93,6 +123,10 @@ const pintarCategoria = async () => {
     const datosLlamada = llamada.datos.products
     console.log(datosLlamada);
 
+    const tituloCategoria = document.createElement('P')
+    tituloCategoria.textContent = `Resultados encontrados por ${categoriaElegida} :`
+    divTituloCategoria.append(tituloCategoria)
+
     datosLlamada.forEach(item => {
 
         const espacioFigure = document.createElement('FIGURE')
@@ -108,6 +142,7 @@ const pintarCategoria = async () => {
         espacioPrecio.textContent = `Precio: ${item.price} €`
 
         const espacioBoton = document.createElement('BUTTON')
+        espacioBoton.classList.add('botonSumaLocal')
         espacioBoton.textContent = 'Añadir'
 
         espacioFigure.append(espacioTitulo, espacioImagen, espacioPrecio, espacioBoton)
@@ -116,6 +151,36 @@ const pintarCategoria = async () => {
 
     });
 
+
+
+}
+
+
+const borrarBusqueda = () => {
+    espacioBoton.style.display = 'none'
+    espacioCategoria.innerHTML = ""
+    divTituloCategoria.innerHTML = ""
+}
+
+
+const guardarLocal = (objeto) => {
+
+    console.log(objeto);
+
+    const coincidenciaLocal = datosLocal.find((item) => objeto == item.nombre)
+
+    console.log(coincidenciaLocal);
+
+    if (!coincidenciaLocal) {
+        datosLocal.push({ nombre: objeto, count: 1 })
+        localStorage.setItem('lista', JSON.stringify(datosLocal))
+    } else {
+        coincidenciaLocal.count++
+        localStorage.setItem('lista', JSON.stringify(datosLocal))
+    }
+
+    // datosLocal.push(objeto)
+    // localStorage.setItem('lista', JSON.stringify(datosLocal))
 }
 
 pintarTodos()
